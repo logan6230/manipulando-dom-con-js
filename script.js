8474bdb5878b0fc459301ccf936828e6a7b43c1b
logan6230 <pantoja2187@gmail.com>
2023-01-05 13:52:58 -0500

    fin clase 03

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+(()=>{
 const btn_agregar = document.querySelector("[data-form-btn]")
 
 
@@ -18,14 +19,34 @@ const createTask = (evento) => {
     const task = document.createElement("li")
     task.classList.add("card")
     input_formulario.value = '';
-    const content = `<div>
-                    <i class="far fa-check-square icon"></i>
-                    <span class="task">${value}</span>
-                    </div>
-                    <i class="fas fa-trash-alt trashIcon icon"></i>`
-    task.innerHTML = content;
+    const taskContent = document.createElement("div");
+
+    const titleTask = document.createElement("span");
+    titleTask.classList.add("task");
+    titleTask.innerText = value;
+    taskContent.appendChild(checkComplete());
+    taskContent.appendChild(titleTask);
+    const content = `<i class="fas fa-trash-alt trashIcon icon"></i>`
+    // task.innerHTML = content;
+    task.appendChild(taskContent)
     list.appendChild(task)
-    console.log(content);
 }
 
 btn_agregar.addEventListener("click", createTask);
+
+const checkComplete = () => {
+    const i = document.createElement("i")
+    i.classList.add("far", "fa-check-square", "icon")
+    i.addEventListener("click", completeTask)
+    return i
+}
+//Immediately invoked function expression IIFE
+//Funciones que se ejecutan en cuanto se declaran
+const completeTask = (event)=>{
+    const element = event.target
+    element.classList.toggle("fas");
+    element.classList.toggle("completeIcon");
+    element.classList.toggle("far");
+}
+
+})()
