98a6c3734b7fe95431e483b45a8852ed86fdb1ca
logan6230 <pantoja2187@gmail.com>
2023-01-04 22:07:38 -0500

    Clase 03 completada

--- script.js
+++ script.js
@@ -13,6 +13,19 @@ const btn_agregar = document.querySelector("[data-form-btn]")
 const createTask = (evento) => {
     evento.preventDefault();
     const input_formulario = document.querySelector("[data-form-input]")
-    console.log(input_formulario.value);
+    const value = input_formulario.value
+    const list = document.querySelector("[data-list]")
+    const task = document.createElement("li")
+    task.classList.add("card")
+    input_formulario.value = '';
+    const content = `<div>
+                    <i class="far fa-check-square icon"></i>
+                    <span class="task">${value}</span>
+                    </div>
+                    <i class="fas fa-trash-alt trashIcon icon"></i>`
+    task.innerHTML = content;
+    list.appendChild(task)
+    console.log(content);
 }
+
 btn_agregar.addEventListener("click", createTask);
